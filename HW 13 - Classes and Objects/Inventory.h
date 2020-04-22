09292d886f183f772992cac79a2660e688c8676c
Matthew Sheehan <matthewsheehan92@gmail.com>
2020-04-22 16:22:59 -0700

    Update Inventory.h

--- HW 13 - Classes and Objects/Inventory.h
+++ HW 13 - Classes and Objects/Inventory.h
@@ -2,7 +2,7 @@
 
 #ifndef INVENTORY_H
 #define INVENTORY_H
-#include <string>
+#include < string >
 using namespace std;
 
 class Inventory {
@@ -15,25 +15,25 @@ class Inventory {
   
  public:
      // Constructors. 
-     Inventory();
-     Inventory(string item_number, string description, int quantity, double cost);
+     Inventory ( );
+     Inventory ( string item_number, string description, int quantity, double cost );
      
      // Mutator methods.
-     void setItemNumber(string item_number);
-     void setDescription(string description);
-     void setQuantity(int quantity);
-     void setCost(double cost);
+     void setItemNumber ( string item_number );
+     void setDescription ( string description );
+     void setQuantity ( int quantity );
+     void setCost ( double cost );
      
      // Accessor methods.
-     string getItemNumber();
-     string getDescription();
-     int getQuantity();
-     double getCost();
+     string getItemNumber ( );
+     string getDescription ( );
+     int getQuantity ( );
+     double getCost ( );
      
      // Calculating item's total cost.
-     double calcTotalCost(); 
+     double calcTotalCost ( ); 
      
      // Prints information on the item.
-     void printItemReport();
+     void printItemReport ( );
 };
 #endif
